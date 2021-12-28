04a1b9ccad5211607ee3d3f5afbfddde84acc454
luuthaituan <luuthaituan08091999@gmail.com>
2021-12-28 13:01:35 +0700

    test2

---
+++ DB.php
@@ -0,0 +1,106 @@
+<?php
+ class DB
+ {
+     protected $db;
+     protected $tableName = 'posts';
+     public function __construct()
+     {
+         $host = 'localhost';
+         $dbname = 'mvc';
+         $username = 'root';
+         $password = 'Tuan@8999';
+    
+         try {
+             $db = new PDO(
+                 "mysql:host=$host;dbname=$dbname;charset=utf8",
+                 $username,
+                 $password
+             );
+             $this->db = $db;
+         } catch (PDOException $e) {
+             echo $e->getMessage();
+         }
+     }
+      
+     public function getData()
+     {
+         $sql = "SELECT * FROM $this->tableName";
+         $result = $this->queryAll($sql);
+         $rows = $result->fetchAll();
+         return $rows;
+     }
+    
+     public function insertData($data = [])
+     {
+         //$data = ['title' => 'title1', 'content' => 'content1' ];
+         $sql = "INSERT INTO $this->tableName "; 
+         $field = null;
+         $values = null;
+         foreach ($data as $fieldName => $value){
+             $field .= "$fieldName, ";
+             $values .= "'$value', ";
+         }
+         $field = trim($field, ", "); //loai ki tu thua 2 dau
+         $values = trim($values, ", ");
+         $sql.= "($field) values ($values) ";
+         $result = $this->queryAll($sql);
+         return $result;
+     }
+    
+     public function deleteData($id)
+     {
+         $sql = "DELETE FROM $this->tableName WHERE id= $id " ;
+         return  $this->queryAll($sql);
+     }
+    
+     public function updateData($id, $data= [])
+     {
+        $sql = "UPDATE $this->tableName SET "; 
+        $setValue = null;
+        foreach ($data as $fieldName => $value){
+            $setValue .= "$fieldName = '$value', ";
+        }
+        $setValue = trim($setValue, ", "); //loai ki tu thua 2 dau
+        $sql.= "$setValue where id = $id";
+        $result = $this->queryAll($sql);
+        return $result;
+     }
+     public function queryAll($sql) {
+        $start = microtime(true) * 1000;
+        $result =  $this->db->query($sql);
+        $end = microtime(true) * 1000;
+        $executeTime = number_format($end - $start,2);
+        $this->writeLog($executeTime, $sql);
+        return $result;
+     }
+
+     public function writeLog($executeTime, $sql){
+        $logs = [
+            'logMessage' => 'SLOW SQL',
+            'logDateTime' => date('Y-m-d H:i:s'),
+            'data' => ['sql' => $sql, 'time' => "$executeTime ms"]
+        ];
+        $logs = json_encode($logs);
+        $query = 'query_';
+        if($executeTime > 50){
+            $query = 'slow_';
+        }
+        
+        if(!file_exists(dirname(__DIR__).'/assign3/logs/mysql')){
+            mkdir(dirname(__DIR__).'/assign3/logs/mysql', 0777);
+        }
+        $path = dirname(__DIR__).'/assign3/logs/mysql/'.$query.date('Ymd_His').'.log';
+        $path = str_replace('/', '\\', $path);
+        file_put_contents($path, $logs);
+    }
+
+
+ }
+ $db = new DB();
+ 
+
+ 
+ $db->getData();
+ //$db->updateData(2 ,['title' => "bai 3454", 'content' => "noi dung 3454" ]);
+//$db->deleteData(6);
+ //$db->insertData(['title' => "bai 3454", 'content' => "noi dung 3454" ]);
